5d3c61e5d41223b48c2d0f4b69823a84fae6afa1
Zlobin Peter <zlobin_peter@bk.ru>
2021-03-06 21:05:27 +0300

    update1

--- MyHabits/MainCollectionView/HabitsViewController.swift
+++ MyHabits/MainCollectionView/HabitsViewController.swift
@@ -31,51 +31,53 @@ class HabitsViewController: UIViewController {
     }
 }
 
-
 // MARK: tableChanges
 
 extension HabitsViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let progressCell = mainTable.dequeueReusableCell(withReuseIdentifier: "progressCell", for: indexPath) as! ViewCellProgress
+        progressCell.layer.cornerRadius = 4
+        progressCell.progressBar.progress = HabitsStore.shared.todayProgress
+        progressCell.labelOfprogress.text = HabitsStore.shared.todayProgress.description
+        return progressCell
+    }
+    
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 2
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        switch section {
-        case 0:
-            return 1
-        default:
-            return HabitsStore.shared.habits.count
-        }
+        return HabitsStore.shared.habits.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        switch indexPath.section {
-        case 0:
-            let progressCell = mainTable.dequeueReusableCell(withReuseIdentifier: "progressCell", for: indexPath) as! ViewCellProgress
-            progressCell.layer.cornerRadius = 4
-            progressCell.progressBar.progress = HabitsStore.shared.todayProgress
-            progressCell.labelOfprogress.text = HabitsStore.shared.todayProgress.description
-            return progressCell
-        default:
-            let reUsebleCell = mainTable.dequeueReusableCell(withReuseIdentifier: "cellMain", for: indexPath) as! collectionViewCellMain
-            reUsebleCell.mainLabel.text = HabitsStore.shared.habits[indexPath.item].name
-            reUsebleCell.everyDayAt.text = "Каждый день в"
-            reUsebleCell.oneByOne.text = "Подряд"
-            reUsebleCell.numberOfOneByOne.text = HabitsStore.shared.habits[indexPath.item].trackDates.description
-            return reUsebleCell
+        let reUsebleCell = mainTable.dequeueReusableCell(withReuseIdentifier: "cellMain", for: indexPath) as! collectionViewCellMain
+        reUsebleCell.mainLabel.text = HabitsStore.shared.habits[indexPath.item].name
+        reUsebleCell.everyDayAt.text = "Каждый день в"
+        reUsebleCell.oneByOne.text = "Подряд"
+        reUsebleCell.numberOfOneByOne.text = HabitsStore.shared.habits[indexPath.item].trackDates.description
+        return reUsebleCell
+    }
+}
+    
+    extension HabitsViewController: CreateCellDelegate {
+        
+        func addNewCell() {
             
+            mainTable.reloadData()
+            return
         }
     }
-}
-
-extension HabitsViewController: CreateCellDelegate {
     
-    func addNewCell() {
+    extension HabitsViewController: UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
         
-        mainTable.reloadData()
-        return
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: view.frame.width - 32 , height: 130)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+            return CGSize(width: view.frame.width - 32 , height: 60)
+        }
     }
-}
-
